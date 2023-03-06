88be48b56c13f8f783aa2b3e756e77ab0297d085
ABDUL QADIR SYED <abdulqadirsyed168@gmail.com>
2023-03-06 17:47:39 +0530

    Add existing project files to Git

---
+++ Kafka_Assignment/kafka_json_consumer_2.py
@@ -0,0 +1,127 @@
+import argparse
+
+from confluent_kafka import Consumer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+
+
+API_KEY = 'WLOKHCU6PDMZNHVC'
+ENDPOINT_SCHEMA_URL  = 'https://psrc-30dr2.us-central1.gcp.confluent.cloud'
+API_SECRET_KEY = 'Fs8lJPMAQhYjxfDawy7jVUI/fO/rOPBexWQeZauwMLQyY44zW1MWg0X20K6FGnMe'
+BOOTSTRAP_SERVER = 'pkc-lzvrd.us-west4.gcp.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'HT3IX7DIHATKMTWC'
+SCHEMA_REGISTRY_API_SECRET = 'IT9SiyCqaEZCFttPscGYbJ7C4LvJ1cvrKGFb0qX7oATGd2SQMtRsnnR1142qBMMh'
+
+
+def sasl_conf():
+
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                #  'security.protocol': 'SASL_PLAINTEXT'}
+                'bootstrap.servers':BOOTSTRAP_SERVER,
+                'security.protocol': SECURITY_PROTOCOL,
+                'sasl.username': API_KEY,
+                'sasl.password': API_SECRET_KEY
+                }
+    return sasl_conf
+
+
+
+def schema_config():
+    return {'url':ENDPOINT_SCHEMA_URL,
+    
+    'basic.auth.user.info':f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+    }
+
+
+class Car:   
+    def __init__(self,record:dict):
+        for k,v in record.items():
+            setattr(self,k,v)
+        
+        self.record=record
+   
+    @staticmethod
+    def dict_to_car(data:dict,ctx):
+        return Car(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+
+def main(topic):
+
+    schema_str = """
+    {
+  "$id": "http://example.com/myURI.schema.json",
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "additionalProperties": false,
+  "description": "Sample schema to help you get started.",
+  "properties": {
+    "Order Number": {
+      "description": "The type(v) type is used.",
+      "type": "number"
+    },
+    "Order Date": {
+      "description": "The type(v) type is used.",
+      "type": "string"
+    },
+    "Item Name": {
+      "description": "The type(v) type is used.",
+      "type": "string"
+    },
+    "Quantity": {
+      "description": "The type(v) type is used.",
+      "type": "number"
+    },
+    "Product Price": {
+      "description": "The type(v) type is used.",
+      "type": "number"
+    },
+    "Total products": {
+      "description": "The type(v) type is used.",
+      "type": "number"
+    }
+  },
+  "title": "SampleRecord",
+  "type": "object"
+}
+    """
+   #reading the latest version of schema and schema_str from schema registry and useing it for data deserialization. 
+    schema_registry_conf = schema_config()
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    schema_name = schema_registry_client.get_schema(100004).schema_str
+    json_deserializer = JSONDeserializer(schema_name,
+                                         from_dict=Car.dict_to_car)
+
+    consumer_conf = sasl_conf()
+    consumer_conf.update({
+                     'group.id': 'group1',
+                     'auto.offset.reset': "latest"})
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic])
+
+
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+
+            car = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+
+            if car is not None:
+                print("User record {}: car: {}\n"
+                      .format(msg.key(), car))
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
+
+main("restaurent-take-away-data")
